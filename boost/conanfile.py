9dc7065a8c9f57966c0967fe94f4132362fa8af9
Xi Guo <guoxi54@outlook.com>
2024-12-07 21:12:38 +0800

    feat: add boost

---
+++ boost/conanfile.py
@@ -0,0 +1,39 @@
+from conan import ConanFile
+from conan.tools.apple import fix_apple_shared_install_name
+from conan.tools.cmake import CMake, CMakeToolchain, CMakeDeps
+from conan.tools.files import get
+
+class boostRecipe(ConanFile):
+    name = "boost"
+    description = "Boost provides free peer-reviewed portable C++ source libraries"
+    license = "BSD-3-Clause"
+    generators = "CMakeDeps", "CMakeToolchain"
+    no_copy_source = True
+    settings = "os", "arch", "compiler", "build_type"
+    options = {"shared": [True, False], "fPIC": [True, False]}
+    default_options = {"shared": False, "fPIC": True}
+    version = "boost-1.86.0"
+    url = "https://github.com/boostorg/boost/releases/download/{0}/{0}-cmake.tar.gz".format(version)
+
+    def source(self):
+        get(self, self.url, strip_root=True)
+
+    def configure_cmake(self):
+        cmake = CMake(self)
+        def convert_to_cmake_boolean(value):
+            return "ON" if value else "OFF"
+
+        variables = {}
+        variables["BUILD_SHARED_LIBS"] = convert_to_cmake_boolean(self.options.shared)
+        variables["BUILD_STATIC_LIBS"] = convert_to_cmake_boolean(not self.options.shared)
+        variables["CMAKE_CXX_STANDARD"] = self.settings.compiler.cppstd
+        variables["CMAKE_POSITION_INDEPENDENT_CODE"] = convert_to_cmake_boolean(True)
+
+        cmake.configure(variables=variables)
+        return cmake
+
+    def build(self):
+        cmake = self.configure_cmake()
+        cmake.build()
+        cmake.install()
+        fix_apple_shared_install_name(self)
