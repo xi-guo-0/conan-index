2272e1350e1b721c1dc518bbe6a933dd8c0361ef
Xi Guo <guoxi54@outlook.com>
2024-12-14 10:07:17 +0800

    feat: onnx

---
+++ onnx/conanfile.py
@@ -0,0 +1,39 @@
+from conan import ConanFile
+from conan.tools.apple import fix_apple_shared_install_name
+from conan.tools.cmake import CMake, CMakeToolchain, CMakeDeps
+from conan.tools.files import get
+
+class onnxRecipe(ConanFile):
+    name = "onnx"
+    description = "Open standard for machine learning interoperability"
+    license = "Apache-2.0"
+    generators = "CMakeDeps", "CMakeToolchain"
+    no_copy_source = True
+    settings = "os", "arch", "compiler", "build_type"
+    options = {"shared": [True, False], "fPIC": [True, False]}
+    default_options = {"shared": False, "fPIC": True}
+    version = "v1.17.0"
+    url = "https://github.com/onnx/onnx/archive/refs/tags/{0}.tar.gz".format(version)
+
+    def source(self):
+        get(self, self.url, strip_root=True)
+
+    def configure_cmake(self):
+        cmake = CMake(self)
+        def convert_to_cmake_boolean(value):
+            return "ON" if value else "OFF"
+
+        variables = {}
+        variables["BUILD_SHARED_LIBS"] = convert_to_cmake_boolean(self.options.shared)
+        variables["BUILD_STATIC_LIBS"] = convert_to_cmake_boolean(not self.options.shared)
+        variables["CMAKE_CXX_STANDARD"] = self.settings.compiler.cppstd
+        variables["CMAKE_POSITION_INDEPENDENT_CODE"] = convert_to_cmake_boolean(True)
+
+        cmake.configure(variables=variables)
+        return cmake
+
+    def build(self):
+        cmake = self.configure_cmake()
+        cmake.build()
+        cmake.install()
+        fix_apple_shared_install_name(self)
