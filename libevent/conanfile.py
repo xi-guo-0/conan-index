012af5db1d959967db3f581a83aab12d917f6f62
Xi Guo <guoxi54@outlook.com>
2024-12-15 17:12:00 +0800

    feat: libevent

---
+++ libevent/conanfile.py
@@ -0,0 +1,42 @@
+from conan import ConanFile
+from conan.tools.apple import fix_apple_shared_install_name
+from conan.tools.cmake import CMake, CMakeToolchain, CMakeDeps
+from conan.tools.files import get
+
+class libeventRecipe(ConanFile):
+    name = "libevent"
+    description = "Event notification library"
+    license = "BSD-3-Clause"
+    generators = "CMakeDeps", "CMakeToolchain"
+    no_copy_source = True
+    settings = "os", "arch", "compiler", "build_type"
+    options = {"shared": [True, False], "fPIC": [True, False]}
+    default_options = {"shared": False, "fPIC": True}
+    version = "release-2.1.12-stable"
+    url = "https://github.com/libevent/libevent/archive/refs/tags/{0}.tar.gz".format(version)
+
+    def source(self):
+        get(self, self.url, strip_root=True)
+
+    def configure_cmake(self):
+        cmake = CMake(self)
+        def convert_to_cmake_boolean(value):
+            return "ON" if value else "OFF"
+
+        variables = {}
+        variables["BUILD_SHARED_LIBS"] = convert_to_cmake_boolean(self.options.shared)
+        variables["BUILD_STATIC_LIBS"] = convert_to_cmake_boolean(not self.options.shared)
+        variables["CMAKE_CXX_STANDARD"] = self.settings.compiler.cppstd
+        variables["CMAKE_POSITION_INDEPENDENT_CODE"] = convert_to_cmake_boolean(True)
+
+        variables["EVENT__LIBRARY_TYPE"] = "SHARED" if self.options.shared else "STATIC"
+        variables["EVENT__DISABLE_OPENSSL"] = convert_to_cmake_boolean(True)
+
+        cmake.configure(variables=variables)
+        return cmake
+
+    def build(self):
+        cmake = self.configure_cmake()
+        cmake.build()
+        cmake.install()
+        fix_apple_shared_install_name(self)
