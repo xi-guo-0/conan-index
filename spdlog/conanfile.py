7b5b48edd0cf889db1e4e17843ede7420a8e9a37
Xi Guo <guoxi54@outlook.com>
2024-12-15 17:22:20 +0800

    feat: spdlog

---
+++ spdlog/conanfile.py
@@ -0,0 +1,42 @@
+from conan import ConanFile
+from conan.tools.apple import fix_apple_shared_install_name
+from conan.tools.cmake import CMake, CMakeToolchain, CMakeDeps
+from conan.tools.files import get
+
+class spdlogRecipe(ConanFile):
+    name = "spdlog"
+    description = "Fast C++ logging library"
+    license = "MIT"
+    generators = "CMakeDeps", "CMakeToolchain"
+    no_copy_source = True
+    settings = "os", "arch", "compiler", "build_type"
+    options = {"shared": [True, False], "fPIC": [True, False]}
+    default_options = {"shared": False, "fPIC": True}
+    version = "v1.15.0"
+    url = "https://github.com/gabime/spdlog/archive/refs/tags/{0}.tar.gz".format(version)
+
+    def source(self):
+        get(self, self.url, strip_root=True)
+
+    def configure_cmake(self):
+        cmake = CMake(self)
+        def convert_to_cmake_boolean(value):
+            return "ON" if value else "OFF"
+
+        variables = {}
+        variables["BUILD_SHARED_LIBS"] = convert_to_cmake_boolean(self.options.shared)
+        variables["BUILD_STATIC_LIBS"] = convert_to_cmake_boolean(not self.options.shared)
+        variables["CMAKE_CXX_STANDARD"] = self.settings.compiler.cppstd
+        variables["CMAKE_POSITION_INDEPENDENT_CODE"] = convert_to_cmake_boolean(True)
+
+        variables["SPDLOG_BUILD_SHARED"] = convert_to_cmake_boolean(self.options.shared)
+        variables["SPDLOG_BUILD_PIC"] = convert_to_cmake_boolean(self.options.fPIC)
+
+        cmake.configure(variables=variables)
+        return cmake
+
+    def build(self):
+        cmake = self.configure_cmake()
+        cmake.build()
+        cmake.install()
+        fix_apple_shared_install_name(self)
